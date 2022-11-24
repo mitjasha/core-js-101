98e0ad8fbb8bb6017e2203384abf7aa63c608d58
mitjasha <mitjasha@gmail.com>
2022-11-24 23:40:55 +0300

    feat: add promises tasks solution

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -27,8 +27,20 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer === true) {
+      resolve('Hooray!!! She said "Yes"!');
+    }
+
+    if (isPositiveAnswer === false) {
+      resolve('Oh no, she said "No".');
+    }
+
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
 
 /**
@@ -69,8 +81,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array).then((firstValue) => firstValue);
 }
 
 /**
